9010d344681346113d1cbe3419bae117a70deacf
Eric Zanchi <emzanchi@gmail.com>
2017-03-08 16:20:40 -0600

    Updated db scripts to exit on completion

--- db/dropDb.js
+++ db/dropDb.js
@@ -2,4 +2,6 @@ const server = require('../db.json');
 const r = require('rethinkdbdash')(server);
 
 r.dbDrop('retro').run()
-.then(result => console.log(result));
+.then(result => console.log(result))
+// This lets the script exit after the db has been dropped
+.then(_ => r.getPoolMaster().drain());
--- db/setupDb.js
+++ db/setupDb.js
@@ -2,16 +2,20 @@ const server = require('../db.json');
 const r = require('rethinkdbdash')(server);
 
 // Set up db
-var created = r.dbCreate('retro').run();
+const created = r.dbCreate('retro').run();
 
 /* Create sprints table */
-created.then(_ => r.db('retro').tableCreate('sprints', {primaryKey: 'name'}).run())
+const sprints = created.then(_ => r.db('retro').tableCreate('sprints', {primaryKey: 'name'}).run())
   .then(_ => r.table('sprints').indexCreate('end').run())
   .then(result => console.log(result))
   .catch(err => console.log(err));
 
 /* Create comments table */
-created.then(_ => r.db('retro').tableCreate('comments').run())
+const comments = created.then(_ => r.db('retro').tableCreate('comments').run())
   .then(_ => r.table('comments').indexCreate('sprint').run())
   .then(result => console.log(result))
   .catch(err => console.log(err));
+
+// This lets the script exit after the tables have been created
+Promise.all([sprints, comments])
+.then(_ => r.getPoolMaster().drain());
