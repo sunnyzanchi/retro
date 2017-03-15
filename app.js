5ec437114744dd9a835b6ee6b0c939b8057e3bf4
Eric Zanchi <emzanchi@gmail.com>
2017-03-15 14:33:45 -0500

    Added app level error-handling

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 app.use('/api', api);
 app.use(router);
 
+/* Error handling */
+app.use(function(err, req, res, next){
+  console.log(err);
+  res.status(500).end();
+});
+
 app.listen(3000);
 
 module.exports = app;
