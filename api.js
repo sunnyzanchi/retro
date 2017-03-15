793ff2d1913f37a5557cb35565d57db9173d30aa
Eric Zanchi <emzanchi@gmail.com>
2017-03-15 14:33:25 -0500

    Updated api to use async/await keywords

--- api.js
+++ api.js
@@ -6,13 +6,12 @@ const server = require('./db.json');
 const r = require('rethinkdbdash')(server);
 
 /* Gets a list of all sprints */
-router.get('/sprints', function(req, res){
-  r.table('sprints')
-    .orderBy({index: 'end'})
-    .pluck('name', 'start', 'end')
-    .run()
-  .then(result => res.json(result))
-  .catch(err => res.status(500).send(err));
+router.get('/sprints', async function(req, res){
+  const sprints = await r.table('sprints')
+                          .orderBy({index: 'end'})
+                          .pluck('name', 'start', 'end')
+                          .run();
+  return res.json(sprints);
 });
 
 /*
@@ -21,44 +20,44 @@ router.get('/sprints', function(req, res){
  * PUT: Creates a new sprint with the given information
  */
 router.route('/sprints/:name')
-  .get(function(req, res){
+  .get(async function(req, res){
     const name = req.params.name;
 
-    const meta = r.table('sprints')
-      .filter({name})
-      .pluck('name', 'start', 'end')
-      .run();
+    const pMeta = r.table('sprints')
+                    .filter({name})
+                    .pluck('name', 'start', 'end')
+                    .run();
 
-    const comments = r.table('comments')
-      .filter({sprint: name})
-      .pluck('comment', 'type')
-      .run();
+    const pComments = r.table('comments')
+                        .filter({name})
+                        .pluck('comment', 'type')
+                        .run();
 
-    Promise.all([meta, comments])
-    .then(function(values){
-      const [meta, comments] = values;
+    const [meta, comments] = await Promise.all([pMeta, pComments]);
+    if(meta.length === 0)
+      return res.status(404).send('Can\'t find that sprint');
 
-      if(meta.length === 0) return res.status(404).send('Can\'t find that sprint');
-
-      return res.json({meta: meta[0], comments});
-    })
-    .catch(err => res.status(500).send(err));
+    return res.json({meta: meta[0], comments});
   })
 
-  .put(function(req, res){
+  .put(async function(req, res){
     const start = moment(req.body.start);
     const end = moment(req.body.end);
 
     if(!start.isValid() || !end.isValid()) return res.status(400).send('Invalid date');
     if(!end.isAfter(start)) return res.status(400).send('End date must be after start date');
+
     let data = {
       start: start.toDate(),
       end: end.toDate(),
       name: req.params.name
     };
-    r.table('sprints').insert(data, {conflict: 'error'}).run()
-      .then(_ => res.status(201).end())
-      .catch(err => res.status(500).send(err));
+
+    await r.table('sprints')
+            .insert(data, {conflict: 'error'})
+            .run()
+
+    return res.status(201).end();
   });
 
 /*
@@ -67,21 +66,24 @@ router.route('/sprints/:name')
  * POST: Adds comments to the given sprint
  */
 router.route('/sprints/:name/comments')
-  .get(function(req, res){
+  .get(async function(req, res){
      const name = req.params.name;
 
-     r.table('comments').filter({name}).pluck('name', 'comments', 'user').run()
-     .then(result => res.json(result))
-     .catch(err => res.status(500).send(err));
+     const comments = await r.table('comments')
+                              .filter({name})
+                              .pluck('name', 'comment', 'user')
+                              .run()
+
+    return res.json(comments);
    })
 
   .post(async function(req, res){
     const name = req.params.name;
 
     const [{end}] = await r.table('sprints')
-      .filter({name})
-      .pluck('end')
-      .run();
+                            .filter({name})
+                            .pluck('end')
+                            .run();
 
     if(moment(end).diff(moment(), 'days') < -7)
       return res.status(403).send('Cannot add comments more than a week after sprint has ended!');
@@ -94,9 +96,11 @@ router.route('/sprints/:name/comments')
       comment.name = name;
     }
 
-    r.table('comments').insert(comments).run()
-      .then(_ => res.status(201).end())
-      .catch(err => res.status(500).send(err));
+    await r.table('comments')
+            .insert(comments)
+            .run();
+
+    return res.status(201).end();
   });
 
 module.exports = router;
